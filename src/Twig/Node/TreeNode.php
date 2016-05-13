42b96914bd2410c44448b51254cb72a620894348
alain tiemblo <ninsuo@gmail.com>
2016-05-13 08:54:56 +0200

    moved exception to the right place

--- src/Twig/Node/TreeNode.php
+++ src/Twig/Node/TreeNode.php
@@ -22,10 +22,6 @@ class TreeNode extends \Twig_Node
             ->addDebugInfo($this)
         ;
 
-        if (version_compare(PHP_VERSION, '5.4.0', '<')) {
-            throw new \LogicException('The {%tree%} Twig tag requires PHP version 5.4 or higher');
-        }
-
         // $tree_treeA = function($data) use (&$context, &$tree_treeA) {
         $compiler
             ->write("\$tree_")
